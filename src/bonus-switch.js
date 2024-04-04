d637b71d25f00dbce95d121f9f534cf0bf1f6515
RyJaxxon <ryanjaxxon@gmail.com>
2024-04-03 23:32:13 -0400

    Bonus answered

--- src/bonus-switch.js
+++ src/bonus-switch.js
@@ -1,8 +1,36 @@
-const measureRainSwitch = () => {
+const measureRainSwitch = (inches) => {
+  switch (true) {
+    case inches === 0:
+      console.log('drought')
+      break
+    case inches < 2:
+      console.log('dry');
+      break;
+    case inches < 4:
+      console.log('average');
+      break;
+    case inches < 6:
+      console.log('rainy')
+      break;
+    case inches >= 6:
+      console.log('flood')
+      break;
+  }
 };
 
-const rounderSwitch = () => {
-};
+const rounderSwitch = (float, roundSetting) => {
+  switch (true) {
+    case roundSetting === 'up':
+      return Math.ceil(float);
+      break;
+    case roundSetting === 'down':
+      return Math.floor(float);
+      break;
+    case roundSetting === 'honest':
+      return (float - Math.floor(float) < 0.5 ? Math.floor(float) : Math.ceil(float))
+
+  }
+}
 
 module.exports = {
   measureRainSwitch,
